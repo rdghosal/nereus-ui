f9c81244702ce9637cf885ce655d97cae5af12e9
Rahul Ghosal <rdghosal@gmail.com>
2023-07-19 22:33:20 -0400

    chore: update deps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let out = nereus::transform(
         "class MyModel(pydantic.BaseModel):\n\n    id: int\n    value: t.Any\n\n    def say_hello():\n        print('hello!')".to_string(),
-    );
+    ).expect("oh no!");
     dbg!("{}", out);
 }
