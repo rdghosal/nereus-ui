81b30db41bd055b769e7a33e7a3619716691f78c
Rahul Ghosal <rdghosal@gmail.com>
2023-07-23 15:35:57 -0400

    feat: test transform example

--- api/src/main.rs
+++ api/src/main.rs
@@ -12,9 +12,14 @@ impl Actor for MyWs {
 /// Handler for ws::Message message
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        let m = msg.as_ref().unwrap();
+        println!("{m:?}");
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Text(text)) => {
+                let cls_diagram = nereus::transform(text.to_string()).unwrap();
+                ctx.text(cls_diagram)
+            }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             _ => (),
         }
--- client/src/app.ts
+++ client/src/app.ts
@@ -2,5 +2,8 @@ import { WebSocket } from "ws";
 
 const ws = new WebSocket("ws://127.0.0.1:8080/ws/");
 ws.on("open", function () {
-  ws.send("hello world");
+  ws.send("class Test(pydantic.BaseModel):\n\n    id: int");
+});
+ws.on("message", function (msg) {
+  console.log(msg.toString());
 });
